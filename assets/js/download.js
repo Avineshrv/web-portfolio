35ef9df2e15f559dae30526a4003636e1ae1382a
Avineshrv <48782992+Avineshrv@users.noreply.github.com>
2024-05-22 00:25:54 +0530

    resume added in resume page along with downloading option

---
+++ assets/js/download.js
@@ -0,0 +1,26 @@
+document
+  .getElementById('download-detailed-resume')
+  .addEventListener('click', function () {
+    downloadAndOpenPDF('/assets/PDF/Avinesh Kumar Resume Detailed.pdf');
+  });
+
+document
+  .getElementById('download-light-resume')
+  .addEventListener('click', function () {
+    downloadAndOpenPDF('/assets/PDF/Avinesh Kumar Resume Light.pdf');
+  });
+
+document
+  .getElementById('download-dark-resume')
+  .addEventListener('click', function () {
+    downloadAndOpenPDF('/assets/PDF/Avinesh Kumar Resume Dark.pdf');
+  });
+
+function downloadAndOpenPDF(pdfUrl) {
+  window.open(pdfUrl, '_blank');
+
+  var link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = pdfUrl.substring(pdfUrl.lastIndexOf('/') + 1);
+  link.click();
+}
